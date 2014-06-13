968f280bb158d9b6ea1f13c0b2c23a096ff520fb
Andrea Barghigiani <a.barghigiani@gmail.com>
2014-06-13 11:20:45 +0200

    Dichiarazione Plugin

---
+++ tassionomie-custom.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Tassionomie Custom
+Plugin URI: http://wpandmore.info/creare-tassionomie-wordpress/
+Description: Questo plugin mi consente di aggiungere nuove tassionomie per i miei articoli e CPT
+Version: 0.1
+Author: Andrea Barghigiani
+Author URI: http://andreabarghigiani.info
+License: GPL2
+*/
