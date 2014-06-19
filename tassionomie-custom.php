c1c24d0ebc1531458058404af9b2bf225f7f7b50
Andrea Barghigiani <a.barghigiani@gmail.com>
2014-06-19 13:37:31 +0200

    Creo la Tassionomia

--- tassionomie-custom.php
+++ tassionomie-custom.php
@@ -1,47 +1,45 @@
 <?php
 /*
 Plugin Name: Tassionomie Custom
-Plugin URI: http://wpandmore.info/creare-tassionomie-wordpress/
-Description: Questo plugin mi consente di aggiungere nuove tassionomie per i miei articoli e CPT
+Plugin URI: http://wpandmore.info/sviluppo-tassionomie-custom/
+Description: Questo plugin mi consente di aggiungere nuove tassionomie per i miei articolo e CPT
 Version: 0.1
 Author: Andrea Barghigiani
 Author URI: http://andreabarghigiani.info
 License: GPL2
 */
 
-
-// Registro Tassionomia Custom
 function tassionomia_custom() {
 
-    $labels = array(
-        'name'                       => _x( 'Prodotti', 'Prodotti Generali', 'text_domain' ),
-        'singular_name'              => _x( 'Prodotto', 'Prodotto Generale', 'text_domain' ),
-        'menu_name'                  => __( 'Prodotti', 'text_domain' ),
-        'all_items'                  => __( 'Tutti i Prodotti', 'text_domain' ),
-        'parent_item'                => __( 'Prodotto Parente', 'text_domain' ),
-        'parent_item_colon'          => __( 'Prodotto Parente:', 'text_domain' ),
-        'new_item_name'              => __( 'Nuovo Prodotto', 'text_domain' ),
-        'add_new_item'               => __( 'Aggiungi Nuovo Prodotto', 'text_domain' ),
-        'edit_item'                  => __( 'Modifica Prodotto', 'text_domain' ),
-        'update_item'                => __( 'Aggiorna Prodotto', 'text_domain' ),
-        'separate_items_with_commas' => __( 'Separa termini con virgola', 'text_domain' ),
-        'search_items'               => __( 'Cerca Prodotti', 'text_domain' ),
-        'add_or_remove_items'        => __( 'Aggiungi o Rimuovi Prodotti', 'text_domain' ),
-        'choose_from_most_used'      => __( 'Scegli tra i Prodotti Usati', 'text_domain' ),
-        'not_found'                  => __( 'Non Trovato', 'text_domain' ),
-    );
-    $args = array(
-        'labels'                     => $labels,
-        'hierarchical'               => true, //false crea tag, true crea categorie
-        'public'                     => true, //la tassionomia e' pubblica?
-        'show_ui'                    => true, //genero una UI per gestire la tassionomia?
-        'show_admin_column'          => true, //creo la colonna nei post associati?
-        'show_in_nav_menus'          => true, //da selezionare nel menu?
-        'show_tagcloud'              => false, //creo una widget tagcloud?
-    );
-    register_taxonomy( 'taxonomy', array( 'post' ), $args );
+	$labels = array(
+		'name'                       => _x( 'Prodotti', 'Prodotti Generali', 'text_domain' ),
+		'singular_name'              => _x( 'Prodotto', 'Prodotto Generale', 'text_domain' ),
+		'menu_name'                  => __( 'Prodotti', 'text_domain' ),
+		'all_items'                  => __( 'Tutti i Prodotti', 'text_domain' ),
+		'parent_item'                => __( 'Prodotto Parente', 'text_domain' ),
+		'parent_item_colon'          => __( 'Prodotto Parente:', 'text_domain' ),
+		'new_item_name'              => __( 'Nuovo Prodotto', 'text_domain' ),
+		'add_new_item'               => __( 'Aggiungi Nuovo Prodotto', 'text_domain' ),
+		'edit_item'                  => __( 'Modifica Prodotto', 'text_domain' ),
+		'update_item'                => __( 'Aggiorna Prodotto', 'text_domain' ),
+		'separate_items_with_commas' => __( 'Separa termini con virgola', 'text_domain' ),
+		'search_items'               => __( 'Cerca Prodotti', 'text_domain' ),
+		'add_or_remove_items'        => __( 'Aggiungi o Rimuovi Prodotti', 'text_domain' ),
+		'choose_from_most_used'      => __( 'Scegli tra i Prodotti Usati', 'text_domain' ),
+		'not_found'                  => __( 'Non Trovato', 'text_domain' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true, //false crea tag, true crea categorie
+		'public'                     => true, //la tassionomia e' pubblica?
+		'show_ui'                    => true, //genero una UI per gestire la tassionomia?
+		'show_admin_column'          => true, //creo la colonna nei post associati?
+		'show_in_nav_menus'          => true, //da selezionare nel menu?
+		'show_tagcloud'              => false, //creo una widget tagcloud?
+	);
+	register_taxonomy( 'prodotti', array( 'post' ), $args );
 
 }
 
-// Mi aggancio a 'init'
+// Hook into the 'init' action
 add_action( 'init', 'tassionomia_custom', 0 );
